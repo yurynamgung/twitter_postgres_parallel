cf4be08b15b0b89a9763b7d2a9128100b198a804
Mike Izbicki <mike@izbicki.me>
2022-03-28 03:18:00 -0700

    typo fix

--- load_tweets_batch.py
+++ load_tweets_batch.py
@@ -172,13 +172,7 @@ def insert_tweets(connection, tweets, batch_size=1000):
     '''
     for i,tweet_batch in enumerate(batch(tweets, batch_size)):
         print(datetime.datetime.now(),'insert_tweets i=',i)
-        while True:
-            try:
-                _insert_tweets(connection, tweet_batch)
-            except (sqlalchemy.exc.OperationalError, sqlalchemy.exc.InternalError) as e:
-                print(f"e={e}")
-                continue
-            break
+        _insert_tweets(connection, tweet_batch)
 
 
 def _insert_tweets(connection,input_tweets):
@@ -397,45 +391,38 @@ def _insert_tweets(connection,input_tweets):
     ######################################## 
     # STEP 2: perform the actual SQL inserts
     ######################################## 
-    while True:
-        try:
-            with connection.begin() as trans:
+    with connection.begin() as trans:
 
-                # use the bulk_insert function to insert most of the data
-                bulk_insert(connection, 'users', users)
-                bulk_insert(connection, 'users', users_unhydrated_from_tweets)
-                bulk_insert(connection, 'users', users_unhydrated_from_mentions)
-                bulk_insert(connection, 'tweet_mentions', tweet_mentions)
-                bulk_insert(connection, 'tweet_tags', tweet_tags)
-                bulk_insert(connection, 'tweet_media', tweet_media)
-                bulk_insert(connection, 'tweet_urls', tweet_urls)
+        # use the bulk_insert function to insert most of the data
+        bulk_insert(connection, 'users', users)
+        bulk_insert(connection, 'users', users_unhydrated_from_tweets)
+        bulk_insert(connection, 'users', users_unhydrated_from_mentions)
+        bulk_insert(connection, 'tweet_mentions', tweet_mentions)
+        bulk_insert(connection, 'tweet_tags', tweet_tags)
+        bulk_insert(connection, 'tweet_media', tweet_media)
+        bulk_insert(connection, 'tweet_urls', tweet_urls)
 
-                # the tweets data cannot be inserted using the bulk_insert function because
-                # the geo column requires special SQL code to generate the column;
-                #
-                # NOTE:
-                # in general, it is a good idea to avoid designing tables that require special SQL on the insertion;
-                # it makes your python code much more complicated,
-                # and is also bad for performance;
-                # I'm doing it here just to help illustrate the problems
-                sql = sqlalchemy.sql.text('''
-                INSERT INTO tweets
-                    (id_tweets,id_users,created_at,in_reply_to_status_id,in_reply_to_user_id,quoted_status_id,geo,retweet_count,quote_count,favorite_count,withheld_copyright,withheld_in_countries,place_name,country_code,state_code,lang,text,source)
-                    VALUES
-                    '''
-                    +
-                    ','.join([f"(:id_tweets{i},:id_users{i},:created_at{i},:in_reply_to_status_id{i},:in_reply_to_user_id{i},:quoted_status_id{i},ST_GeomFromText(:geo_str{i} || '(' || :geo_coords{i} || ')'), :retweet_count{i},:quote_count{i},:favorite_count{i},:withheld_copyright{i},:withheld_in_countries{i},:place_name{i},:country_code{i},:state_code{i},:lang{i},:text{i},:source{i})" for i in range(len(tweets))])
-                    +
-                    '''
-                    ON CONFLICT DO NOTHING
-                    '''
-                    )
-                res = connection.execute(sql, { key+str(i):value for i,tweet in enumerate(tweets) for key,value in tweet.items() })
-
-        except sqlalchemy.exc.OperationalError as e:
-            print(f"e={e}")
-            continue
-        break
+        # the tweets data cannot be inserted using the bulk_insert function because
+        # the geo column requires special SQL code to generate the column;
+        #
+        # NOTE:
+        # in general, it is a good idea to avoid designing tables that require special SQL on the insertion;
+        # it makes your python code much more complicated,
+        # and is also bad for performance;
+        # I'm doing it here just to help illustrate the problems
+        sql = sqlalchemy.sql.text('''
+        INSERT INTO tweets
+            (id_tweets,id_users,created_at,in_reply_to_status_id,in_reply_to_user_id,quoted_status_id,geo,retweet_count,quote_count,favorite_count,withheld_copyright,withheld_in_countries,place_name,country_code,state_code,lang,text,source)
+            VALUES
+            '''
+            +
+            ','.join([f"(:id_tweets{i},:id_users{i},:created_at{i},:in_reply_to_status_id{i},:in_reply_to_user_id{i},:quoted_status_id{i},ST_GeomFromText(:geo_str{i} || '(' || :geo_coords{i} || ')'), :retweet_count{i},:quote_count{i},:favorite_count{i},:withheld_copyright{i},:withheld_in_countries{i},:place_name{i},:country_code{i},:state_code{i},:lang{i},:text{i},:source{i})" for i in range(len(tweets))])
+            +
+            '''
+            ON CONFLICT DO NOTHING
+            '''
+            )
+        res = connection.execute(sql, { key+str(i):value for i,tweet in enumerate(tweets) for key,value in tweet.items() })
 
 
 if __name__ == '__main__':
