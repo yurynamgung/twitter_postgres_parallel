07dd13eff164eee549eacf55a7792a856a14cd40
Mike Izbicki <mike@izbicki.me>
2022-03-28 07:00:54 -0700

    fix typo

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -10,9 +10,9 @@ echo '==========================================================================
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:25432/ --inputs
+echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:35432/ --inputs
+echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs
--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -6,15 +6,15 @@ echo '==========================================================================
 echo 'load denormalized'
 echo '================================================================================'
 for file in $files; do
-    time unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:15432/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+    time unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:25432/ --inputs $files
+time python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2/ --inputs $files
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:35432/ --inputs $files
+time python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs $files
